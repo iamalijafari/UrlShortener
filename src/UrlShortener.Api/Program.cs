af45e95367aa27be174f31680f1ed8efe08451eb
iamalijafari <ajsq337@gmail.com>
2026-07-06 00:16:41 +0330

    chore: add Docker support for local development

--- src/UrlShortener.Api/Program.cs
+++ src/UrlShortener.Api/Program.cs
@@ -1,6 +1,8 @@
 using UrlShortener.Application;
 using UrlShortener.Infrastructure;
 using UrlShortener.Api.Middleware;
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +11,22 @@ builder.Services.AddControllers();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services
+    .AddHealthChecks()
+    .AddNpgSql(
+        builder.Configuration.GetConnectionString("DefaultConnection")!,
+        name: "postgres");
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -26,6 +39,8 @@ app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health");
 
 app.Run();
 
